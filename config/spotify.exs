ea8444b86a084ae17fbd36e713a0577df6891f25
jennyshih <jinghua.shih@gmail.com>
2020-05-25 08:19:25 +0800

    reset auth callback url to localhost

--- config/spotify.exs
+++ config/spotify.exs
@@ -2,4 +2,4 @@ use Mix.Config
 
 config :spotify_ex,
   scopes: ["user-top-read", "playlist-read-private", "user-read-private", "user-read-email"],
-  callback_url: "https://3b53a0fb.ngrok.io/authenticate"
+  callback_url: "http://localhost:4000/authenticate"
