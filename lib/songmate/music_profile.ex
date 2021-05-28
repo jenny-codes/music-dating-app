85a9ae5c65e2264b659ed5b7f51ec584ec8cacaf
jennyshih <jinghua.shih@gmail.com>
2021-05-28 09:12:00 +0800

    [refac] remove unused functions in MusicProfile

--- lib/songmate/music_profile.ex
+++ lib/songmate/music_profile.ex
@@ -10,52 +10,16 @@ defmodule Songmate.MusicProfile do
   alias Songmate.Music.{Artist, Track, Genre}
   alias Songmate.Accounts
 
-  @doc """
-  Returns the list of music_profiles.
-
-  ## Examples
-
-      iex> list_music_profiles()
-      [%User{}, ...]
-
-  """
-  def list_music_profiles do
+  def list_profiles do
     Repo.all(Profile)
   end
 
-  @doc """
-  Gets a single profile.
-
-  Raises `Ecto.NoResultsError` if the User does not exist.
-
-  ## Examples
-
-      iex> get_profile!(123)
-      %User{}
-
-      iex> get_profile!(456)
-      ** (Ecto.NoResultsError)
-
-  """
-  def get_profile!(id), do: Repo.get!(Profile, id)
-
-  @doc """
-  Creates a profile.
-
-  ## Examples
-
-      iex> create_profile(%{field: value})
-      {:ok, %User{}}
-
-      iex> create_profile(%{field: bad_value})
-      {:error, %Ecto.Changeset{}}
-
-  """
   def create_profile(attrs \\ %{}) do
-    {:ok, profile} = %Profile{}
-                     |> Profile.changeset(attrs)
-                     |> Ecto.Changeset.cast_assoc(:user, with: &Accounts.User.changeset/2)
-                     |> Repo.insert()
+    {:ok, profile} =
+      %Profile{}
+      |> Profile.changeset(attrs)
+      |> Ecto.Changeset.cast_assoc(:user, with: &Accounts.User.changeset/2)
+      |> Repo.insert()
 
     create_artist_preferences_for_profile(profile, attrs[:artist_preferences])
     create_track_preferences_for_profile(profile, attrs[:track_preferences])
@@ -64,15 +28,20 @@ defmodule Songmate.MusicProfile do
   end
 
   def create_or_update_profile(attrs \\ %{}) do
-    profile = case Repo.preload(attrs[:user], :music_profile).music_profile do
-      nil ->
-        {:ok, profile} = %Profile{}
-                         |> Profile.changeset(attrs)
-                         |> Ecto.Changeset.put_assoc(:user, attrs[:user])
-                         |> Repo.insert()
-        profile
-      profile -> profile
-    end
+    profile =
+      case Repo.preload(attrs[:user], :music_profile).music_profile do
+        nil ->
+          {:ok, profile} =
+            %Profile{}
+            |> Profile.changeset(attrs)
+            |> Ecto.Changeset.put_assoc(:user, attrs[:user])
+            |> Repo.insert()
+
+          profile
+
+        profile ->
+          profile
+      end
 
     create_artist_preferences_for_profile(profile, attrs[:artist_preferences])
     create_track_preferences_for_profile(profile, attrs[:track_preferences])
@@ -80,96 +49,16 @@ defmodule Songmate.MusicProfile do
     profile
   end
 
-  @doc """
-  Updates a profile.
-
-  ## Examples
-
-      iex> update_profile(profile, %{field: new_value})
-      {:ok, %User{}}
-
-      iex> update_profile(profile, %{field: bad_value})
-      {:error, %Ecto.Changeset{}}
-
-  """
   def update_profile(%Profile{} = profile, attrs) do
     profile
     |> Profile.changeset(attrs)
     |> Repo.update()
   end
 
-  @doc """
-  Deletes a profile.
-
-  ## Examples
-
-      iex> delete_profile(profile)
-      {:ok, %User{}}
-
-      iex> delete_profile(profile)
-      {:error, %Ecto.Changeset{}}
-
-  """
   def delete_profile(%Profile{} = profile) do
     Repo.delete(profile)
   end
 
-  @doc """
-  Returns an `%Ecto.Changeset{}` for tracking profile changes.
-
-  ## Examples
-
-      iex> change_profile(profile)
-      %Ecto.Changeset{source: %Profile{}}
-
-  """
-  def change_profile(%Profile{} = profile) do
-    Profile.changeset(profile, %{})
-  end
-
-  alias Songmate.MusicProfile.ArtistPreference
-
-  @doc """
-  Returns the list of artist_preferences.
-
-  ## Examples
-
-      iex> list_artist_preferences()
-      [%ArtistPreference{}, ...]
-
-  """
-  def list_artist_preferences do
-    Repo.all(ArtistPreference)
-  end
-
-  @doc """
-  Gets a single artist_preference.
-
-  Raises `Ecto.NoResultsError` if the Artist preference does not exist.
-
-  ## Examples
-
-      iex> get_artist_preference!(123)
-      %ArtistPreference{}
-
-      iex> get_artist_preference!(456)
-      ** (Ecto.NoResultsError)
-
-  """
-  def get_artist_preference!(id), do: Repo.get!(ArtistPreference, id)
-
-  @doc """
-  Creates a artist_preference.
-
-  ## Examples
-
-      iex> create_artist_preference(%{field: value})
-      {:ok, %ArtistPreference{}}
-
-      iex> create_artist_preference(%{field: bad_value})
-      {:error, %Ecto.Changeset{}}
-
-  """
   def create_artist_preference!(attrs \\ %{}) do
     %ArtistPreference{}
     |> ArtistPreference.changeset(attrs)
@@ -180,6 +69,7 @@ defmodule Songmate.MusicProfile do
 
   def create_artist_preferences_for_profile(profile, nil), do: profile
   def create_artist_preferences_for_profile(profile, []), do: profile
+
   def create_artist_preferences_for_profile(profile, prefs) do
     build_artist_attr = fn pref ->
       %{pref | artist: Repo.get_or_create_by!(Artist, :spotify_id, pref[:artist])}
@@ -195,96 +85,10 @@ defmodule Songmate.MusicProfile do
     |> Enum.map(&create_artist_preference!/1)
   end
 
-  @doc """
-  Updates a artist_preference.
-
-  ## Examples
-
-      iex> update_artist_preference(artist_preference, %{field: new_value})
-      {:ok, %ArtistPreference{}}
-
-      iex> update_artist_preference(artist_preference, %{field: bad_value})
-      {:error, %Ecto.Changeset{}}
-
-  """
-  def update_artist_preference(%ArtistPreference{} = artist_preference, attrs) do
-    artist_preference
-    |> ArtistPreference.changeset(attrs)
-    |> Repo.update()
-  end
-
-  @doc """
-  Deletes a artist_preference.
-
-  ## Examples
-
-      iex> delete_artist_preference(artist_preference)
-      {:ok, %ArtistPreference{}}
-
-      iex> delete_artist_preference(artist_preference)
-      {:error, %Ecto.Changeset{}}
-
-  """
   def delete_artist_preference(%ArtistPreference{} = artist_preference) do
     Repo.delete(artist_preference)
   end
 
-  @doc """
-  Returns an `%Ecto.Changeset{}` for tracking artist_preference changes.
-
-  ## Examples
-
-      iex> change_artist_preference(artist_preference)
-      %Ecto.Changeset{source: %ArtistPreference{}}
-
-  """
-  def change_artist_preference(%ArtistPreference{} = artist_preference) do
-    ArtistPreference.changeset(artist_preference, %{})
-  end
-
-  alias Songmate.MusicProfile.TrackPreference
-
-  @doc """
-  Returns the list of track_preferences.
-
-  ## Examples
-
-      iex> list_track_preferences()
-      [%TrackPreference{}, ...]
-
-  """
-  def list_track_preferences do
-    Repo.all(TrackPreference)
-  end
-
-  @doc """
-  Gets a single track_preference.
-
-  Raises `Ecto.NoResultsError` if the Track preference does not exist.
-
-  ## Examples
-
-      iex> get_track_preference!(123)
-      %TrackPreference{}
-
-      iex> get_track_preference!(456)
-      ** (Ecto.NoResultsError)
-
-  """
-  def get_track_preference!(id), do: Repo.get!(TrackPreference, id)
-
-  @doc """
-  Creates a track_preference.
-
-  ## Examples
-
-      iex> create_track_preference(%{field: value})
-      {:ok, %TrackPreference{}}
-
-      iex> create_track_preference(%{field: bad_value})
-      {:error, %Ecto.Changeset{}}
-
-  """
   def create_track_preference!(attrs \\ %{}) do
     %TrackPreference{}
     |> TrackPreference.changeset(attrs)
@@ -295,6 +99,7 @@ defmodule Songmate.MusicProfile do
 
   def create_track_preferences_for_profile(profile, nil), do: profile
   def create_track_preferences_for_profile(profile, []), do: profile
+
   def create_track_preferences_for_profile(profile, prefs) do
     build_track_attr = fn pref ->
       %{pref | track: Repo.get_or_create_by!(Track, [:isrc, :spotify_id], pref[:track])}
@@ -310,96 +115,6 @@ defmodule Songmate.MusicProfile do
     |> Enum.map(&create_track_preference!/1)
   end
 
-  @doc """
-  Updates a track_preference.
-
-  ## Examples
-
-      iex> update_track_preference(track_preference, %{field: new_value})
-      {:ok, %TrackPreference{}}
-
-      iex> update_track_preference(track_preference, %{field: bad_value})
-      {:error, %Ecto.Changeset{}}
-
-  """
-  def update_track_preference(%TrackPreference{} = track_preference, attrs) do
-    track_preference
-    |> TrackPreference.changeset(attrs)
-    |> Repo.update()
-  end
-
-  @doc """
-  Deletes a track_preference.
-
-  ## Examples
-
-      iex> delete_track_preference(track_preference)
-      {:ok, %TrackPreference{}}
-
-      iex> delete_track_preference(track_preference)
-      {:error, %Ecto.Changeset{}}
-
-  """
-  def delete_track_preference(%TrackPreference{} = track_preference) do
-    Repo.delete(track_preference)
-  end
-
-  @doc """
-  Returns an `%Ecto.Changeset{}` for tracking track_preference changes.
-
-  ## Examples
-
-      iex> change_track_preference(track_preference)
-      %Ecto.Changeset{source: %TrackPreference{}}
-
-  """
-  def change_track_preference(%TrackPreference{} = track_preference) do
-    TrackPreference.changeset(track_preference, %{})
-  end
-
-  alias Songmate.MusicProfile.GenrePreference
-
-  @doc """
-  Returns the list of genre_preferences.
-
-  ## Examples
-
-      iex> list_genre_preferences()
-      [%GenrePreference{}, ...]
-
-  """
-  def list_genre_preferences do
-    Repo.all(GenrePreference)
-  end
-
-  @doc """
-  Gets a single genre_preference.
-
-  Raises `Ecto.NoResultsError` if the Genre preference does not exist.
-
-  ## Examples
-
-      iex> get_genre_preference!(123)
-      %GenrePreference{}
-
-      iex> get_genre_preference!(456)
-      ** (Ecto.NoResultsError)
-
-  """
-  def get_genre_preference!(id), do: Repo.get!(GenrePreference, id)
-
-  @doc """
-  Creates a genre_preference.
-
-  ## Examples
-
-      iex> create_genre_preference(%{field: value})
-      {:ok, %GenrePreference{}}
-
-      iex> create_genre_preference(%{field: bad_value})
-      {:error, %Ecto.Changeset{}}
-
-  """
   def create_genre_preference!(attrs \\ %{}) do
     %GenrePreference{}
     |> GenrePreference.changeset(attrs)
@@ -410,6 +125,7 @@ defmodule Songmate.MusicProfile do
 
   def create_genre_preferences_for_profile(profile, nil), do: profile
   def create_genre_preferences_for_profile(profile, []), do: profile
+
   def create_genre_preferences_for_profile(profile, prefs) do
     build_genre_attr = fn pref ->
       %{pref | genre: Repo.get_or_create_by!(Genre, :name, pref[:genre])}
@@ -423,53 +139,5 @@ defmodule Songmate.MusicProfile do
     |> Enum.map(build_genre_attr)
     |> Enum.map(add_profile_attr)
     |> Enum.map(&create_genre_preference!/1)
-
-  end
-
-  @doc """
-  Updates a genre_preference.
-
-  ## Examples
-
-      iex> update_genre_preference(genre_preference, %{field: new_value})
-      {:ok, %GenrePreference{}}
-
-      iex> update_genre_preference(genre_preference, %{field: bad_value})
-      {:error, %Ecto.Changeset{}}
-
-  """
-  def update_genre_preference(%GenrePreference{} = genre_preference, attrs) do
-    genre_preference
-    |> GenrePreference.changeset(attrs)
-    |> Repo.update()
-  end
-
-  @doc """
-  Deletes a genre_preference.
-
-  ## Examples
-
-      iex> delete_genre_preference(genre_preference)
-      {:ok, %GenrePreference{}}
-
-      iex> delete_genre_preference(genre_preference)
-      {:error, %Ecto.Changeset{}}
-
-  """
-  def delete_genre_preference(%GenrePreference{} = genre_preference) do
-    Repo.delete(genre_preference)
-  end
-
-  @doc """
-  Returns an `%Ecto.Changeset{}` for tracking genre_preference changes.
-
-  ## Examples
-
-      iex> change_genre_preference(genre_preference)
-      %Ecto.Changeset{source: %GenrePreference{}}
-
-  """
-  def change_genre_preference(%GenrePreference{} = genre_preference) do
-    GenrePreference.changeset(genre_preference, %{})
   end
 end
--- test/songmate/music_profile_test.exs
+++ test/songmate/music_profile_test.exs
@@ -4,116 +4,120 @@ defmodule Songmate.MusicProfileTest do
   alias Songmate.MusicProfile
   alias Songmate.MusicProfile.Profile
 
-  describe "music_profiles" do
-    def profile_fixture(attrs \\ %{}) do
-      MusicProfile.create_profile(attrs)
-    end
+  def profile_fixture(attrs \\ %{}) do
+    MusicProfile.create_profile(attrs)
+  end
 
-    test "list_music_profiles/0 returns all music_profiles" do
+  describe "list_profiles/0" do
+    test "returns all music_profiles" do
       profile = profile_fixture()
-      assert MusicProfile.list_music_profiles() == [profile]
+      assert MusicProfile.list_profiles() == [profile]
     end
+  end
 
-    test "get_profile!/1 returns the profile with given id" do
-      profile = profile_fixture()
-      assert MusicProfile.get_profile!(profile.id) == profile
-    end
-
-    test "create_profile/1 with valid profile association creates a profile" do
+  describe "create_profile/1 " do
+    test "with valid profile association creates a profile" do
       profile = MusicProfile.create_profile(%{user: valid_user_attrs()})
 
       assert profile.user.id
       assert profile.user.name == "Bass Wannabe"
     end
 
-    test "create_profile/1 with artists preference associations creates associations" do
-      profile = MusicProfile.create_profile(
-        %{
+    test "with artists preference associations creates associations" do
+      profile =
+        MusicProfile.create_profile(%{
           user: valid_user_attrs(),
-          artist_preferences: [%{
-            rank: 1,
-            artist: valid_artist_attrs()
-          }]
-        }
-      )
+          artist_preferences: [
+            %{
+              rank: 1,
+              artist: valid_artist_attrs()
+            }
+          ]
+        })
 
-      artist_pref = profile
-                    |> Repo.preload(artist_preferences: [:artist])
-                    |> fn profile -> profile.artist_preferences end.()
-                    |> List.first()
+      artist_pref =
+        profile
+        |> Repo.preload(artist_preferences: [:artist])
+        |> (fn profile -> profile.artist_preferences end).()
+        |> List.first()
 
       assert artist_pref.rank == 1
       assert artist_pref.artist.name == "9m88"
     end
 
-    test "create_profile/1 with track preference associations creates associations" do
-      profile = MusicProfile.create_profile(
-        %{
+    test "with track preference associations creates associations" do
+      profile =
+        MusicProfile.create_profile(%{
           user: valid_user_attrs(),
-          track_preferences: [%{
-            rank: 1,
-            track: valid_track_attrs()
-          }]
-        }
-      )
+          track_preferences: [
+            %{
+              rank: 1,
+              track: valid_track_attrs()
+            }
+          ]
+        })
 
-      track_pref = profile
-                   |> Repo.preload(track_preferences: [:track])
-                   |> fn profile -> profile.track_preferences end.()
-                   |> List.first()
+      track_pref =
+        profile
+        |> Repo.preload(track_preferences: [:track])
+        |> (fn profile -> profile.track_preferences end).()
+        |> List.first()
 
       assert track_pref.rank == 1
       assert track_pref.track.name == "Rebellion (Lies)"
     end
 
-    test "create_profile/1 with genre preference associations creates associations" do
-      profile = MusicProfile.create_profile(
-        %{
+    test "with genre preference associations creates associations" do
+      profile =
+        MusicProfile.create_profile(%{
           user: valid_user_attrs(),
-          genre_preferences: [%{
-            rank: 1,
-            genre: valid_genre_attrs()
-          }]
-        }
-      )
+          genre_preferences: [
+            %{
+              rank: 1,
+              genre: valid_genre_attrs()
+            }
+          ]
+        })
 
-      genre_pref = profile
-                   |> Repo.preload(genre_preferences: [:genre])
-                   |> fn profile -> profile.genre_preferences end.()
-                   |> List.first()
+      genre_pref =
+        profile
+        |> Repo.preload(genre_preferences: [:genre])
+        |> (fn profile -> profile.genre_preferences end).()
+        |> List.first()
 
       assert genre_pref.rank == 1
       assert genre_pref.genre.name == "Modern Rock"
     end
+  end
 
-    test "delete_profile/1 deletes the user profile" do
+  describe "delete_profile/1" do
+    test "deletes the user profile" do
       profile = profile_fixture()
       assert {:ok, %Profile{}} = MusicProfile.delete_profile(profile)
-      assert_raise Ecto.NoResultsError, fn -> MusicProfile.get_profile!(profile.id) end
+      assert_raise Ecto.NoResultsError, fn -> Repo.get!(Profile, profile.id) end
     end
 
     test "delete Account.User deletes associated profile and preferences" do
-      profile = profile_fixture(
-        %{
+      profile =
+        profile_fixture(%{
           user: valid_user_attrs(),
-          artist_preferences: [%{
-            rank: 1,
-            artist: valid_artist_attrs()
-          }]
-        }
-      )
+          artist_preferences: [
+            %{
+              rank: 1,
+              artist: valid_artist_attrs()
+            }
+          ]
+        })
 
       MusicProfile.delete_profile(profile)
 
-      assert Repo.all(from(ap in MusicProfile.ArtistPreference, where: ap.music_profile_id == ^profile.id)) == []
+      assert Repo.all(
+               from(ap in MusicProfile.ArtistPreference, where: ap.music_profile_id == ^profile.id)
+             ) == []
+
       # Make sure the associated user and artist are not deleted.
       assert length(Repo.all(Songmate.Music.Artist)) == 1
       assert Repo.get!(Songmate.Accounts.User, Repo.preload(profile, :user).user.id)
-    end
-
-    test "change_profile/1 returns a profile changeset" do
-      profile = profile_fixture()
-      assert %Ecto.Changeset{} = MusicProfile.change_profile(profile)
     end
   end
 end
