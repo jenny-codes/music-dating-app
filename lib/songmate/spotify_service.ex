9e109da1b639ec341e14ee39691fc29c549be58a
jennyshih <jinghua.shih@gmail.com>
2021-05-29 17:17:36 +0800

    [refac] move SpotifyService under namespace Songmate

---
+++ lib/songmate/spotify_service.ex
@@ -0,0 +1,76 @@
+defmodule Songmate.SpotifyService do
+  alias Songmate.Music.{Artist, Track, Genre}
+
+  # ---------------------------------------------------------
+  # Auths
+  def validate_and_refresh_token(conn) do
+    with true <- Spotify.Authentication.tokens_present?(conn),
+         {:ok, conn} <- Spotify.Authentication.refresh(conn) do
+      {:ok, conn}
+    end
+  end
+
+  # ---------------------------------------------------------
+  # Music
+  def fetch_user_info(conn) do
+    {:ok, profile} = Spotify.Profile.me(conn)
+
+    [
+      email: profile.email,
+      username: profile.id,
+      avatar_url: List.first(profile.images)["url"],
+      display_name: profile.display_name
+    ]
+  end
+
+  def fetch_music_preferences(conn) do
+    {artists, genres} = fetch_top_artists_and_genres(conn)
+    tracks = fetch_top_tracks(conn)
+
+    [tracks: tracks, artists: artists, genres: genres]
+  end
+
+  @spec fetch_top_tracks(%{__struct__: Plug.Conn | Spotify.Credentials}) :: [%Track{}]
+  defp fetch_top_tracks(conn) do
+    {:ok, %{items: tracks}} =
+      Spotify.Personalization.top_tracks(conn, limit: 50, time_range: "medium_term")
+
+    # There is an "artists" field for each track, but we don't parse that for now.
+    Enum.map(tracks, fn track ->
+      %Track{
+        name: track.name,
+        isrc: track.external_ids["isrc"],
+        spotify_id: track.id,
+        popularity: track.popularity
+      }
+    end)
+  end
+
+  @spec fetch_top_artists_and_genres(%{__struct__: Plug.Conn | Spotify.Credentials}) ::
+          {[%Artist{}], [%Genre{}]}
+  defp fetch_top_artists_and_genres(conn) do
+    {:ok, %{items: artists}} =
+      Spotify.Personalization.top_artists(conn, limit: 50, time_range: "medium_term")
+
+    artist_records =
+      Enum.map(artists, fn artist ->
+        %Artist{
+          name: artist.name,
+          spotify_id: artist.id,
+          popularity: artist.popularity
+        }
+      end)
+
+    genre_records =
+      artists
+      |> Enum.flat_map(& &1.genres)
+      |> Enum.uniq()
+      |> Enum.map(fn genre ->
+        %Genre{
+          name: genre
+        }
+      end)
+
+    {artist_records, genre_records}
+  end
+end
