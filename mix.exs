b31769ba12dd2e9984357c8e04f1c1e948c488d4
jennyshih <jinghua.shih@gmail.com>
2021-06-19 17:25:46 +0800

    [build] introduce Mox testing library

--- mix.exs
+++ mix.exs
@@ -45,7 +45,8 @@ defmodule Songmate.MixProject do
       {:plug_cowboy, "~> 2.3"},
       {:spotify_ex, "~> 2.0.9"},
       {:ecto_enum, "~> 1.4"},
-      {:hackney, "~> 1.16"}
+      {:hackney, "~> 1.16"},
+      {:mox, "~> 1.0", only: :test}
     ]
   end
 
